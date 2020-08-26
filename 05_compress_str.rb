4f52a5529172672001bc025c3123ba56972d14bb
bd2187 <brandon.dionisio.26@gmail.com>
2020-08-25 19:56:00 -0700

    Create compress_str

--- 05_compress_str.rb
+++ 05_compress_str.rb
@@ -2,6 +2,33 @@
 # The method should return a new str where streaks of consecutive characters are compressed.
 # For example "aaabbc" is compressed to "3a2bc".
 
+def compress_str(str)
+    # loop through str
+    # if current i is the same as (i + 1),
+    # increment count. otherwise, restart count
+
+    compressed_str = ""
+    count = 1
+    (0...str.length).each do |i|        
+        if str[i] == str[i + 1]
+
+            count += 1
+
+        elsif count == 1
+
+            compressed_str += str[i].to_s
+
+        else
+
+            compressed_str += count.to_s + str[i].to_s
+            count = 1
+            
+        end
+    end
+
+    compressed_str
+end
+
 
 p compress_str("aaabbc")        # => "3a2bc"
 p compress_str("xxyyyyzz")      # => "2x4y2z"
