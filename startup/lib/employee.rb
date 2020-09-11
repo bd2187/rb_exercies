f77ed18255e93008069e7647cfa16a46f95f68c3
bd2187 <brandon.dionisio.26@gmail.com>
2020-09-10 21:45:36 -0700

    Create Employee class

--- startup/lib/employee.rb
+++ startup/lib/employee.rb
@@ -1,3 +1,28 @@
 class Employee
 
+    attr_accessor :name, :title
+    attr_reader :earnings
+
+    def initialize(name, title)
+        @name = name
+        @title = title
+        @earnings = 0
+    end
+
+    def name
+        @name
+    end
+
+    def title
+        @title
+    end
+
+    def pay(amount)
+        @earnings += amount
+    end
 end
+
+employee = Employee.new("Shaggy", "Junior Developer")
+employee.pay(42)
+employee.pay(50)
+p employee.earnings
